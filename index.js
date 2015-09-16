4b6ec9e0c43140c42286ed815547e0b0aa2d4a15
Janos Haber <janos.haber@finesolution.hu>
2015-09-16 15:29:25 +0200

    Sync copy task

--- index.js
+++ index.js
@@ -7,58 +7,54 @@ var os = require('os');
  * It is called only one time by Bower, to instantiate resolver.
  * You can instantiate here any caches or create helper functions.
  */
-module.exports = function resolver (bower) {
+module.exports = function resolver(bower) {
 
-  // Resolver factory returns an instance of resolver
-  return {
+    // Resolver factory returns an instance of resolver
+    return {
 
-    // Match method tells whether resolver supports given source
-    // It can return either boolean or promise of boolean
-    match: function (source) {
-        return fs.isDirectoryAsync(path.resolve(source))
-        .catch(function() {
-            return false;
-        });
-    },
+        // Match method tells whether resolver supports given source
+        // It can return either boolean or promise of boolean
+        match: function (source) {
+            return fs.isDirectoryAsync(path.resolve(source))
+                .catch(function () {
+                    return false;
+                });
+        },
 
-    // Optional:
-    // Can resolve or normalize sources, like:
-    // "jquery" => "git://github.com/jquery/jquery.git"
-    locate: function (source) {
-        return path.resolve(source);
-    },
+        // Optional:
+        // Can resolve or normalize sources, like:
+        // "jquery" => "git://github.com/jquery/jquery.git"
+        locate: function (source) {
+            return path.resolve(source);
+        },
 
-    // Optional:
-    // Allows to list available versions of given source.
-    // Bower chooses matching release and passes it to "fetch"
-    releases: function (source) {
-        return fs.readJsonAsync(path.join(source, 'bower.json'))
-        .then(function(bw) {
-            return [{
-                target: bw.version,
-                version: bw.version
-            }];
-        })
-        .catch(function(err) {
-            return [];
-        });
-    },
+        // Optional:
+        // Allows to list available versions of given source.
+        // Bower chooses matching release and passes it to "fetch"
+        releases: function (source) {
+            return fs.readJsonAsync(path.join(source, 'bower.json'))
+                .then(function (bw) {
+                    return [{
+                        target: bw.version,
+                        version: bw.version
+                    }];
+                })
+                .catch(function (err) {
+                    return [];
+                });
+        },
 
-    // It downloads package and extracts it to temporary directory
-    // You can use npm's "tmp" package to tmp directories
-    // See the "Resolver API" section for details on this method
-    fetch: function (endpoint, cached) {
-        var tempPath = path.join(os.tmpdir(), endpoint.name);
-        return fs.copyAsync(endpoint.source, tempPath)
-        .then(function() {
+        // It downloads package and extracts it to temporary directory
+        // You can use npm's "tmp" package to tmp directories
+        // See the "Resolver API" section for details on this method
+        fetch: function (endpoint, cached) {
+            var tempPath = path.join(os.tmpdir(), endpoint.name);
+            fs.copy(endpoint.source, tempPath);
             return {
                 tempPath: tempPath,
-                removeIgnores: true
+                removeIgnores: true,
+                resolution: new Date()
             };
-        })
-        .catch(function() {
-            return;
-        });
-    }
-  };
+        }
+    };
 };
